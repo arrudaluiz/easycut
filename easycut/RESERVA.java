60a8dd1d51e7f26ff3b3eed7ab5b27ec4a5c7fd0
megaaza <39173683+megaaza@users.noreply.github.com>
2018-05-16 22:27:21 -0300

    Add files via upload

---
+++ easycut/RESERVA.java
@@ -0,0 +1,55 @@
+package easycut;
+
+public class RESERVA {
+
+	private int RES_ID;
+
+	private String RES_HORARIO;
+
+	private String RES_OBS;
+
+	private String RES_CLOSED;
+
+    public int getRES_ID() {
+        return RES_ID;
+    }
+
+    public void setRES_ID(int RES_ID) {
+        this.RES_ID = RES_ID;
+    }
+
+    public String getRES_HORARIO() {
+        return RES_HORARIO;
+    }
+
+    public void setRES_HORARIO(String RES_HORARIO) {
+        this.RES_HORARIO = RES_HORARIO;
+    }
+
+    public String getRES_OBS() {
+        return RES_OBS;
+    }
+
+    public void setRES_OBS(String RES_OBS) {
+        this.RES_OBS = RES_OBS;
+    }
+
+    public String getRES_CLOSED() {
+        return RES_CLOSED;
+    }
+
+    public void setRES_CLOSED(String RES_CLOSED) {
+        this.RES_CLOSED = RES_CLOSED;
+    }
+
+	public void VIS_RESERV() {
+
+	}
+
+	public void GRAV_RESERV() {
+
+	}
+
+}
+
+
---
+++ easycut/USUARIO.java
@@ -0,0 +1,83 @@
+package easycut;
+
+public class USUARIO {
+
+	private int USU_COD;
+
+    public int getUSU_COD() {
+        return USU_COD;
+    }
+
+    public void setUSU_COD(int USU_COD) {
+        this.USU_COD = USU_COD;
+    }
+
+    public String getUSU_LOGIN() {
+        return USU_LOGIN;
+    }
+
+    public void setUSU_LOGIN(String USU_LOGIN) {
+        this.USU_LOGIN = USU_LOGIN;
+    }
+
+    public String getUSU_PASSWORD() {
+        return USU_PASSWORD;
+    }
+
+    public void setUSU_PASSWORD(String USU_PASSWORD) {
+        this.USU_PASSWORD = USU_PASSWORD;
+    }
+
+    public String getUSU_NAME() {
+        return USU_NAME;
+    }
+
+    public void setUSU_NAME(String USU_NAME) {
+        this.USU_NAME = USU_NAME;
+    }
+
+    public String getUSU_CPF() {
+        return USU_CPF;
+    }
+
+    public void setUSU_CPF(String USU_CPF) {
+        this.USU_CPF = USU_CPF;
+    }
+
+    public boolean isUSU_TYPE() {
+        return USU_TYPE;
+    }
+
+    public void setUSU_TYPE(boolean USU_TYPE) {
+        this.USU_TYPE = USU_TYPE;
+    }
+
+    public boolean isUSU_REMOVE() {
+        return USU_REMOVE;
+    }
+
+    public void setUSU_REMOVE(boolean USU_REMOVE) {
+        this.USU_REMOVE = USU_REMOVE;
+    }
+
+	private String USU_LOGIN;
+
+	private String USU_PASSWORD;
+
+	private String USU_NAME;
+
+	private String USU_CPF;
+
+	private boolean USU_TYPE;
+
+	private boolean USU_REMOVE;
+
+	public int GRAV_USER() {
+		return 0;
+	}
+
+	public void SELECT_USER() {
+
+	}
+
+}
